8a124405ff3f4d9a1298f28a3424cce4c36a030b
Archit Ghevariya <ghevariyaarchit@gmail.com>
2022-08-22 00:34:09 +0530

    Login page added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 var nav = responsiveNav(".nav-collapse");
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+      $("p").hide();
+    });
+    $("#show").click(function(){
+      $("p").show();
+    });
+  });
