4495eab88980273851056ad6fc85051774f46c1b
Archit Ghevariya <ghevariyaarchit@gmail.com>
2022-08-22 21:04:42 +0530

    vie product page modified

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ $(document).ready(function(){
       $("p").show();
     });
   });
+
+
+// Up down nmbers
+
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
